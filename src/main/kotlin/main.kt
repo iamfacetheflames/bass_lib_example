41830ad104eeca4f11578afd340c590d193d0c2d
Face The-Flames <facetheflames@yandex.ru>
2021-11-24 23:54:45 +0700

    Просто меняем позицию и громкость трека.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,15 @@
 
 import jouvieje.bass.Bass
 import jouvieje.bass.BassInit
+import jouvieje.bass.defines.BASS_POS.BASS_POS_MUSIC_ORDER
 import jouvieje.bass.defines.BASS_SAMPLE
 import jouvieje.bass.exceptions.BassException
 import jouvieje.bass.structures.HSAMPLE
 import java.io.File
+import jouvieje.bass.Bass.BASS_ChannelSeconds2Bytes
+import jouvieje.bass.defines.BASS_ATTRIB.BASS_ATTRIB_VOL
+import jouvieje.bass.defines.BASS_POS.BASS_POS_BYTE
+
 
 var init: Boolean = false
 
@@ -37,11 +42,18 @@ fun main(args: Array<String>) {
     } else {
         val testAudioFile = File( "test.mp3")
 //        Bass.BASS_SetVolume(1f)
-        val sam: HSAMPLE = Bass.BASS_SampleLoad(false, testAudioFile.absolutePath, 0, 0, 3, BASS_SAMPLE.BASS_SAMPLE_OVER_POS)
-        val channel = Bass.BASS_SampleGetChannel(sam, false)
+        val handle: HSAMPLE = Bass.BASS_SampleLoad(false, testAudioFile.absolutePath, 0, 0, 3, BASS_SAMPLE.BASS_SAMPLE_OVER_POS)
+        val channel = Bass.BASS_SampleGetChannel(handle, false)
         if (!Bass.BASS_ChannelPlay(channel.asInt(), false)) {
             error("Can't play sample")
         }
+
+        // seek to 8.7 sec
+        val prebuf = BASS_ChannelSeconds2Bytes(channel.asInt(), 8.7)
+        Bass.BASS_ChannelSetPosition(channel.asInt(), prebuf, BASS_POS_BYTE)
+
+        // set volume to 0.5f for 15 sec
+        Bass.BASS_ChannelSlideAttribute(channel.asInt(), BASS_ATTRIB_VOL, 0.5f, 15000)
     }
     readLine()
 }
